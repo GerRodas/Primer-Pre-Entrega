0e3def8315918b94ecbb79d28e8b6aef1921f63f
German <germanrodas@gmail.com>
2022-12-15 18:39:32 -0300

    agregadas plantillas y websockets, aun no funciona bien

--- src/app.js
+++ src/app.js
@@ -1,16 +1,44 @@
 import express from 'express'
-
+import handlebars from 'express-handlebars';
+import __dirname from '../src/utils.js';
+import viewsRouter from './routes/views.router.js'
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
-import Handlebars from 'handlebars';
 import { Server } from 'socket.io';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+//app.use(express.json());
+//app.use(express.urlencoded({extended: true}));
 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
-app.use('/',(req,res) => res.send('ESTA ES LA PAGINA PARA COMPRAR COSITAS'));
 
-app.listen(8080);
+const httpServer = app.listen(8080, () => console.log("Listening on PORT 8080"));
+const socketServer = new Server(httpServer);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'/views');
+app.set('view engine','handlebars');
+app.use(express.static(__dirname+'/public'));
+app.use('/',viewsRouter);
+
+//app.use('/api/products', productsRouter);
+//app.use('/api/carts', cartsRouter);
+
+
+socketServer.on('connection', socket=>{
+    console.log("Nuevo cliente conectado")
+    socket.on('message', data=>{
+        console.log(data);
+
+    })
+})
+
+
+/*
+app.get('/', (req, res)=>{
+    let testUser={
+        name: "Hilda",
+        last_name:"Martinez"
+    }
+    res.render('index', testUser);
+})
+*/
